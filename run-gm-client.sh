00efccb83a5b0b38e2f2c2f7021ffa983732b821
Nico Hagelberg <nicou@nicou.me>
2024-06-26 09:09:41 +0300

    Add run scripts

---
+++ run-gm-client.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+HOME_PATH="/home/gm/ee-gm-client-home"
+
+HOME="$HOME_PATH" _build/EmptyEpsilon
---
+++ run-server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+HOME_PATH="/home/gm/ee-server-home"
+
+HOME="$HOME_PATH" _build/EmptyEpsilon
